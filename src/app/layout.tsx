e993b31a4b694dd36d5d56a7ddf9ca650e6b60d9
jaceuk <jnewington@gmail.com>
2023-06-02 12:31:03 +0100

    Added GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,13 @@ import '@css/normalize.css';
 import '@css/reset.css';
 import '@css/typography.scss';
 import '@css/global.scss';
+import Script from 'next/script';
 import { Poppins } from 'next/font/google';
 import Header from '@/components/shared/Header';
 import Footer from '@/components/shared/Footer';
 
 const poppins = Poppins({ weight: ['300', '400', '600', '700', '900'], subsets: ['latin'] });
+const GA_MEASUREMENT_ID = 'G-VFXHZ7Q5NS';
 
 export const metadata = {
   title: 'Jason Newington - helping make the web a better place',
@@ -22,6 +24,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {children}
         <Footer />
       </body>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
     </html>
   );
 }

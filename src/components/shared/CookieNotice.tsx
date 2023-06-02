491c453cca1c561d9bc85c5c94f345e6144c6b07
jaceuk <jnewington@gmail.com>
2023-06-02 16:35:50 +0100

    Fixed localStorage issue

--- src/components/shared/CookieNotice.tsx
+++ src/components/shared/CookieNotice.tsx
@@ -6,21 +6,31 @@ import Button from '@/components/shared/Button';
 import InnerWrapper from '@/components/shared/InnerWrapper';
 
 export default function CookieNotice() {
-  const consent = JSON.parse(window.localStorage.getItem('cookieConsent') || '{}');
-  const [cookieConsent, setCookieConsent] = React.useState(consent.value || false);
+  const [cookieConsent, setCookieConsent] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
 
   function handleAccept() {
     setCookieConsent(!cookieConsent);
   }
 
   React.useEffect(() => {
-    const consent = { value: cookieConsent, timestamp: new Date().getTime() };
-    window.localStorage.setItem('cookieConsent', JSON.stringify(consent));
+    const consent = localStorage.getItem('cookieConsent');
+    if (consent) {
+      setCookieConsent(true);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (cookieConsent) {
+      const cookieDetails = { value: cookieConsent, timestamp: new Date().getTime() };
+      localStorage.setItem('cookieConsent', JSON.stringify(cookieDetails));
+    }
+    setLoading(false);
   }, [cookieConsent]);
 
   return (
     <>
-      {!cookieConsent && (
+      {!cookieConsent && !loading && (
         <div className={styles.cookieNotice}>
           <InnerWrapper>
             <div className={styles.content}>
